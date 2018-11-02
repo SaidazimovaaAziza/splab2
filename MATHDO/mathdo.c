e4ee5baaf2d24436662eb2c283d58459f4b6e8ea
SaidazimovaaAziza <43564854+SaidazimovaaAziza@users.noreply.github.com>
2018-11-02 13:17:45 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -1,23 +1,59 @@
-#include <stdio.h>
 #include <sys/types.h>
-#include <unistd.h>
 #include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]){
-	int pid = fork(); // child1
-	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
-
-	} else // parent
-	{
-		int pid2 = fork();
-		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
-		} else // parent
-		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
-		}		
-	}
+int main(int argc,char* argv[]){
+	char *arguments[5];
+	for (int i=0;i<4;i++){
+    int children = fork();
+    if (children == 0){
+    if (i==0){
+    arguments[0] = "python";
+    arguments[1] = "./addition.py";
+    arguments[2] = argv[1];
+    arguments[3] = argv[2];
+    arguments[4] = NULL;
+    execvp("python",arguments);
+    return 0;}
+		else if(i==1){
+		arguments[0] = "python";
+    arguments[1] = "./substruction.py";
+    arguments[2] = argv[1];
+    arguments[3] = argv[2];
+    arguments[4] = NULL;
+    execvp("python",arguments);
+    return 0;}
+		else if (i==2){
+		arguments[0] = "python";
+    arguments[1] = "./multiplication.py";
+    arguments[2] = argv[1];
+    arguments[3] = argv[2];
+    arguments[4] = NULL;
+    execvp("python",arguments);
+    return 0;
+		}
+		else if (i==3){
+		arguments[0] = "bash";
+    arguments[1] = "division.sh";
+    arguments[2] = argv[1];
+    arguments[3] = argv[2];
+    arguments[4] = NULL;
+    execvp("bash",arguments);
+    execvp("./division.sh",arguments);
+    return 0;
+		}
+    exit(0);
+  }
+  else{
+  wait(NULL);
+}}
+wait(NULL);
+wait(NULL);
+wait(NULL);
+wait(NULL);
+printf("parent: done\n");
 }
+
+
